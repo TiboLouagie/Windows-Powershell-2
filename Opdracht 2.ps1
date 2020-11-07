7c3dde42cd53190741628b23e6fa72f1ffc1e77f
unknown <Administrator@mijnschool.be>
2020-11-07 15:41:59 +0100

    Webserver intranet finished

--- Opdracht 2.ps1
+++ Opdracht 2.ps1
@@ -42,7 +42,7 @@ New-PSDrive -Name "P" -PSProvider FileSystem -Root "C:\Public" -Persist
 
 #endregion
 #======Optional======
-#region Making webserver
+#region Intranet webserver
 
 #Installing IIS on MS
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
@@ -61,8 +61,45 @@ $html = @"
 "@
 
 $service = Get-Service | ConvertTo-Html -Fragment
-ConvertTo-Html -Body $html -Title IIS | Out-File C:\inetpub\intranet\index.html
+ConvertTo-Html -Body $html -Title IIS | Out-File C:\intranet\intranet\index.html
 
 #creating new site
+New-WebSite -Name intranet -IPAddress 192.168.1.4 -Port 80 -HostHeader intranet.mijnschool.be -PhysicalPath "C:\intranet\intranet"
+#Remove-Website -Name intranet
+
+#NDS-record creation (for DC1)
+Add-DnsServerResourceRecordA -Name intranet -iPv4Address 192.168.1.4 -ZoneName intranet.mijnschool.be -ComputerName 192.168.1.2 -CreatePtr
+
+#NTFS (back on MS)
+    #disable inheritance
+    $folder = 'C:\Public'
+    $acl = Get-ACL -Path $folder
+    $acl.SetAccessRuleProtection($True, $True)
+    Set-Acl -Path $folder -AclObject $acl
+
+    #adding personeel
+    $Folderpath='C:\Public'
+    $user_account='MIJNSCHOOL\personeel'
+    $Acl = Get-Acl $Folderpath
+    $Ar = New-Object system.Security.AccessControl.FileSystemAccessRule($user_account, "Modify", "ContainerInherit, ObjectInherit", "None", "Allow")
+    $Acl.Setaccessrule($Ar)
+    Set-Acl $Folderpath $Acl
+
+    #removing Users group
+    $Folderpath='C:\inetpub\intranet'
+    $user_account='Users'
+    $Acl = Get-Acl $Folderpath
+    $Ar = New-Object system.Security.AccessControl.FileSystemAccessRule($user_account, "ReadAndExecute", "ContainerInherit, ObjectInherit", "None", "Allow")
+    $Acl.RemoveAccessRuleAll($Ar)
+    Set-Acl $Folderpath $Acl
+
+    #install windows authentication
+    Install-WindowsFeature web-windows-auth
+    
+    #enable windows authentication
+    Set-WebConfigurationProperty -filter "/system.webServer/security/authentication/windowsAuthentication" -name enabled -value true -PSPath "IIS:\" -location 'intranet'
+    
+    #disable anonymous authentication
+    Set-WebConfigurationProperty -filter /system.WebServer/security/authentication/AnonymousAuthentication -name enabled -value false -PSPath "IIS:\" -location 'intranet'
 
 #endregion
